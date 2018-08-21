1044808c24526aaac31749c52ba7f88aedbfec92
Maksym Chukhriaiev <maksym.chukhriaiev@onseo.biz>
2018-08-21 15:35:57 +0300

    ES6 syntax examples added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { rootId, title } from '../config';
 
+import loop, { A, B } from './es6';
+console.log(loop, A, B);
+
 const App = ({ title }) => (<h1>{ title }</h1>);
 
 const root = document.getElementById(rootId);
